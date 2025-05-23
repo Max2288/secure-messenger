b287e487fae38405de6cfbc18ac131bf93d42fdd
Max Bezborodov <bezborodovms@wlc.capital>
2025-05-23 06:37:07 +0300

    Дополнен декоратор на стандартные ошибки

--- backend/fastapi/src/app/utils/exceptions/decorator.py
+++ backend/fastapi/src/app/utils/exceptions/decorator.py
@@ -1,14 +1,15 @@
-from collections.abc import Callable
+from typing import Callable, Any
 from functools import wraps
-from typing import Any
-
 from fastapi import HTTPException, status
+import structlog
+
 from src.app.utils.exceptions.schema import DomainError, ItemNotFound
 
+logger = structlog.get_logger(__name__)
 
-def handle_domain_error(func: Callable[[Any], Any]) -> Any:
+def handle_domain_error(func: Callable[[Any], Any]) -> Callable[[Any], Any]:
     @wraps(func)
-    async def wrapper(*args: tuple[Any], **kwargs: dict[Any, Any]) -> Callable[[Any], Any]:
+    async def wrapper(*args: Any, **kwargs: Any) -> Any:
         try:
             return await func(*args, **kwargs)
         except ItemNotFound as error:
@@ -20,6 +21,14 @@ def handle_domain_error(func: Callable[[Any], Any]) -> Any:
             raise HTTPException(
                 status_code=status.HTTP_400_BAD_REQUEST,
                 detail=str(error),
+            )
+        except HTTPException as http_error:
+            raise http_error
+        except Exception as error:
+            logger.error("Unhandled exception in handler", error = error)
+            raise HTTPException(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                detail="Internal Server Error"
             )
 
     return wrapper
